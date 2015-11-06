5d3082bde961d2ceef9da4bbfd295d2ddaf5c3f4
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-06 11:38:32 +0000

    Basic structure

--- farm.rb
+++ farm.rb
@@ -1,7 +1,33 @@
-
 #sow seeds
 #pass one year
 #harvest crops
 #sell crops
 #gold increases
 #buy more seeds - price fluctuates rand
+class Farmer
+
+  attr_accessor :seeds, :seedlings, :crops, :gold
+  
+  def initialize
+    @seeds = 5
+    @seedlings = 0
+    @crops = 0
+    @gold = 0
+  end
+  
+  def pass_one_year
+  end
+  
+  def sow _seeds
+  end
+  
+  def harvest_crops
+  end
+  
+  def sell_crops
+  end
+  
+  def buy_seeds
+  end
+
+end
