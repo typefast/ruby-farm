241a85d94316ca25951e9990ba8b1a641b4887ef
Harry Oliver <someonecancode@yahoo.co.uk>
2015-11-06 12:01:25 +0000

    Add methods, pass_one_year, harvest_crops, sell_crops

--- farm.rb
+++ farm.rb
@@ -6,16 +6,19 @@
 #buy more seeds - price fluctuates rand
 class Farmer
 
-  attr_accessor :seeds, :seedlings, :crops, :gold
+  attr_accessor :seeds, :seedlings, :crops, :harvested_crops, :gold
   
   def initialize
     @seeds = 5
     @seedlings = 0
     @crops = 0
+    @harvested_crops = 0
     @gold = 0
   end
   
   def pass_one_year
+    @crops += @seedlings
+    @seedlings = 0
   end
   
   def sow_seeds
@@ -26,9 +29,14 @@ class Farmer
   end
   
   def harvest_crops
+    @harvested_crops += @crops
+    @crops = 0
   end
   
   def sell_crops
+    sale_price = rand(10) + 1
+    @gold = sale_price * @harvested_crops
+    @harvested_crops = 0
   end
   
   def buy_seeds
@@ -38,4 +46,11 @@ end
 john = Farmer.new
 puts john.seeds
 john.sow_seeds
+john.pass_one_year
+puts john.crops
+john.harvest_crops
+puts john.harvested_crops
+puts "Time to sell"
+john.sell_crops
+puts "Gold: #{john.gold}"
 
